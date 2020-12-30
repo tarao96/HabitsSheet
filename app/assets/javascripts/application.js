1762d07067c28051a7c71b17f57837b05a2645bd
EC2 Default User <ec2-user@ip-172-31-42-43.us-east-2.compute.internal>
2020-12-30 10:21:55 +0000

    modify javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,27 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
-//= require moment
-//= require fullcalendar
- 
-(function () {
-    function eventCalendar() {
-        return ('#calendar').fullCalendar({});
-    };
-    function clearCalendar() {
-        ('#calendar').html('');
-    };
-});
-(document).on('turbolinks:load', function () {
-    eventCalendar();
-});
-(document).on('turbolinks:before-cache', clearCalendar);
-('#calendar').fullCalendar({
-    events: '/events.json'
-});
-
 /*!
  * Raty - A Star Rating Plugin
  *
